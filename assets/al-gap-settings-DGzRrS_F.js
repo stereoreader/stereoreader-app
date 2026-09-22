530b9527259afba60c4045a0e7882e011bbdb388
silent_mantra <silentmantra.ru@gmail.com>
2026-09-22 14:38:31 +0200

    deploy

--- assets/al-gap-settings-DGzRrS_F.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./App-i842JraM.js";import"./index-DaR_hPh1.js";export{m as default};
---
+++ assets/al-width-settings-CgRe5Kql.js
@@ -0,0 +1,1 @@
+import{j as m}from"./App-BOVjRfYa.js";import"./index-CX9ekaNE.js";export{m as default};

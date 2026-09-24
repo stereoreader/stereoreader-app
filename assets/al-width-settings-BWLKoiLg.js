2db9c779bb39050c3ed77be570a731ff1d5ceb12
silent_mantra <silentmantra.ru@gmail.com>
2026-09-24 12:39:52 +0200

    deploy

---
+++ assets/al-width-settings-BWLKoiLg.js
@@ -0,0 +1,1 @@
+import{j as m}from"./App-B2FsosSB.js";import"./index-1ON-b8ME.js";export{m as default};
--- assets/al-width-settings-DQsAOf19.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./App-CK7tRGBq.js";import"./index-Dtnzyi0h.js";export{m as default};
